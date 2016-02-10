dd4f53993a92373c7f9ea0459e378e1227c75a8f
ik0oo <kichigindv@gmail.com>
2016-02-11 01:44:57 +0300

    add routing

--- app/app.js
+++ app/app.js
@@ -1,38 +1,63 @@
 angular
-	.module('app', [])
-	.controller('testCtrl', function ($scope) {
-		$scope.add = function () {
-			$scope.addedItem && $scope.items.push({name: $scope.addedItem})
+	.module('app', ['ngRoute'])
+
+	.controller('todoCtrl', function ($scope) {
+		$scope.title = 'TODO';
+		$scope.add = function() {
+			$scope.addedItem && $scope.items.push({
+				name: $scope.addedItem
+			})
 			$scope.addedItem = ''; // clear field
 		};
-		$scope.remove = function (index) {
+		$scope.remove = function(index) {
 			$scope.items.splice(index, 1);
 		};
 
-		$scope.doAll = function () {
-			$scope.items.map(function (item) {
+		$scope.doAll = function() {
+			$scope.items.map(function(item) {
 				return item.isDone = true;
 			});
 		};
 
-		$scope.undoAll = function () {
-			$scope.items.map(function (item) {
+		$scope.undoAll = function() {
+			$scope.items.map(function(item) {
 				return item.isDone = false;
 			});
 		};
 
-		$scope.removeAll = function () {
+		$scope.removeAll = function() {
 			$scope.items = [];
 		};
 
-		$scope.removeAllDone = function () {
-			$scope.items = $scope.items.filter(function (item) {
+		$scope.removeAllDone = function() {
+			$scope.items = $scope.items.filter(function(item) {
 				return !item.isDone;
 			});
 		};
 
-		$scope.items = [
-			{name: 'firs', isDone: false},
-			{name: 'second', isDone: false}
-		];
+		$scope.items = [{
+			name: 'firs',
+			isDone: false
+		}, {
+			name: 'second',
+			isDone: false
+		}];
 	})
+
+	.controller('aboutCtrl', function ($scope) {
+		$scope.title = 'ABOUT'
+	})
+
+	.config(function($routeProvider) {
+
+		$routeProvider
+			.when('/', {
+				templateUrl: 'app/templates/todo.html',
+				controller: 'todoCtrl'
+			})
+			.when('/about', {
+				template: '<dvi>{{title}}</div>',
+				controller: 'aboutCtrl'
+			})
+
+	});
